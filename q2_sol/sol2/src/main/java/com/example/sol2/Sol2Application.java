480e4e10b10fdee056c9a4d072d5170c070d084b
raja2576 <164044997+raja2576@users.noreply.github.com>
2025-04-02 01:20:21 +0530

    Initial commit - Added solution 2

---
+++ q2_sol/sol2/src/main/java/com/example/sol2/Sol2Application.java
@@ -0,0 +1,13 @@
+package com.example.sol2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sol2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sol2Application.class, args);
+	}
+
+}
---
+++ q2_sol/sol2/src/test/java/com/example/sol2/Sol2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sol2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sol2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
