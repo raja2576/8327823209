f372c799c91db34eb28bd053aa205f32d796e173
raja2576 <164044997+raja2576@users.noreply.github.com>
2025-04-02 01:17:42 +0530

    Initial commit - Added solution 1

---
+++ q1_sol/sol1/src/main/java/com/example/sol1/Sol1Application.java
@@ -0,0 +1,13 @@
+package com.example.sol1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sol1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sol1Application.class, args);
+	}
+
+}
---
+++ q1_sol/sol1/src/test/java/com/example/sol1/Sol1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sol1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sol1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
